c3da69ca8a71604f182dfef3820b4e73d1aaa056
Ricardo Bahillo <ricky.bahillo@gmail.com>
2014-07-25 18:12:52 +0200

    Second production version

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -34,6 +34,25 @@ environments {
 			username = "ba0746fabc43cf"
 			password = "bdfcd724"
 			driverClassName = "com.mysql.jdbc.Driver"
+			properties {
+				jmxEnabled = true
+				initialSize = 5
+				maxActive = 50
+				minIdle = 5
+				maxIdle = 25
+				maxWait = 10000
+				maxAge = 10 * 60000
+				timeBetweenEvictionRunsMillis = 5000
+				minEvictableIdleTimeMillis = 60000
+				validationQuery = "SELECT 1"
+				validationQueryTimeout = 3
+				validationInterval = 15000
+				testOnBorrow = true
+				testWhileIdle = true
+				testOnReturn = false
+				jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+				defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+			 }
 			
         }
     }
