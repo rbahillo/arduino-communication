d5198c88f1f28e15009f3a7e62013e22a8297720
Ricardo Bahillo <ricky.bahillo@gmail.com>
2014-07-25 17:42:31 +0200

    Second production version

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,7 +30,9 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:mysql://ba0746fabc43cf:bdfcd724@us-cdbr-east-06.cleardb.net/heroku_c2917b8d728802a?reconnect=true"
+            url = "jdbc:mysql://us-cdbr-east-06.cleardb.net/heroku_c2917b8d728802a?reconnect=true"
+			username = "ba0746fabc43cf"
+			password = "bbdfcd724"
 			driverClassName = "com.mysql.jdbc.Driver"
 			
         }
