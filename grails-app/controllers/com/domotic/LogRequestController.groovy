53f479acb5a8d6760c9ef3c9ecd9a01f187e2314
Ricardo Bahillo <ricky.bahillo@gmail.com>
2014-07-26 22:22:17 +0200

    Second production version

--- grails-app/controllers/com/domotic/LogRequestController.groovy
+++ grails-app/controllers/com/domotic/LogRequestController.groovy
@@ -146,7 +146,7 @@ class LogRequestController {
 				logRequest.dispositivo.save flush:true
 			}
 		}
-		render "<?xml version="1.0" encoding="UTF-8"?><Response></Response>"
+		render "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response></Response>"
 	}
 
     protected void notFound() {
